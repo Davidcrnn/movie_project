de8ad5f9af8c15d0f7a776bd15e47dc9c1b1a9b5
David Crenin <david.crenin@gmail.com>
2020-02-10 07:50:12 +0100

    new project

---
+++ main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import HomePageView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+
+
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
